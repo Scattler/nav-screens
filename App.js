1631f5de3a68e2d3ac550126a78be389594130a7
503 <503@academicos.cesde.edu.co>
2023-04-27 20:03:26 -0500

    Lito para el taller final

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native'
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Contacts from './screens/Contacts';
 //import Loguin from './screens/Loguin';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialIcons } from '@expo/vector-icons'
 import { TextInput, Button } from 'react-native-paper';
 import { useForm, Controller } from 'react-hook-form';
+import { useState } from 'react';
 import Banner from './Componets/Banner.js';
 
 
 //crear constante para generar las rutas de las pantallas(screens)
 
 let users = [
-  {username: 'hgj',name:'anderson',password:22,role:1},
-  {username: 'gfhd',name:'anderson',password:22,role:1}
+  { useremail: 'andersonyepesbedoya@gmail.com', name: 'anderson', password: "22", role: 1 },
+  { useremail: 'aladeus@gmail.com', name: 'alejandro', password: "11", role: 2 }
 ]
 
 
@@ -23,17 +24,17 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
 
-  const {} = useForm();
+  const { } = useForm();
 
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home' screenOptions={{
-        headerStyle:{
-          backgroundColor:"red"
+        headerStyle: {
+          backgroundColor: "red"
         },
-        headerTintColor:"white"
+        headerTintColor: "white"
       }}>
-        <Stack.Screen name="HomeTabs" component={HomeTabs} options={{title:'DAVIVIENDA'}}/>
+        <Stack.Screen name="HomeTabs" component={HomeTabs} options={{ title: 'DAVIVIENDA' }} />
         {/* <Stack.Screen name="Home" component={HomeScreen} options={{title:'Inicio'}}/>
         <Stack.Screen name="Contacts" component={Contacts} options=      {{title:'Contactanos'}}/>
         <Stack.Screen name='Loguin'component={Loguin} options={{title:'Loguin'}}/> */}
@@ -41,82 +42,108 @@ export default function App() {
     </NavigationContainer>
   );
 }
-function HomeScreen({navigation}){
-  return(
+function HomeScreen({ navigation }) {
+
+  const [email, setEmail] = useState('');
+  const [password, setpassword] = useState('');
+  const [errormess, seterrormess] = useState('');
+  const [vacio, setvacio] = useState('');
+
+
+  return (
     <View style={styles.container}>
       <Image
-        style = {styles.imgstyle}
+        style={styles.imgstyle}
         source={require('./assets/Arriba.svg')}
       />
-        <Text style={{marginBottom:20,fontWeight:"bold",fontSize:20}}>INGRESA LOS DATOS DEL USUARIO</Text>
-            <TextInput
-              label="Nombre:"
-              mode='Flat'
-              left={<TextInput.Icon icon="account-box" />}
-              style={{marginTop:20,backgroundColor:"#dcdcdc"}}
-              // onBlur={onBlur}
-              // onChangeText={onChange}
-              // value={value}
-            />
-            <TextInput
-              label="Numero de documento:"
-              mode='flat'
-              left={<TextInput.Icon icon="content-copy" />}
-              style={{marginTop:25,backgroundColor:"#dcdcdc"}}
-              // onBlur={onBlur}
-              // onChangeText={onChange}
-              // value={value}
-            />
+      <Text style={{ marginBottom: 20, fontWeight: "bold", fontSize: 20 }}>INGRESA LOS DATOS DEL USUARIO</Text>
+      <TextInput
+        label="Correo electronico"
+        mode='Flat'
+        right={<TextInput.Icon icon="account-box" />}
+        style={{ marginTop: 20, backgroundColor: "#dcdcdc" }}
+        // onBlur={onBlur}
+        onChangeText={email => setEmail(email)}
+        value={email}
+      />
+      <TextInput
+        label="Password"
+        mode='flat'
+        right={<TextInput.Icon icon="eye" />}
+        style={{ marginTop: 25, backgroundColor: "#dcdcdc" }}
+        // onBlur={onBlur}
+        onChangeText={password => setpassword(password)}
+        value={password}
+        secureTextEntry  //PARA NO DEJAR VER LO QUE SE ESTA ESCRIBIENDO EN LA CONTRASEÑA, POR DEFECTO VIENE EN TRUE
+      />
 
-              <Button 
-                style={{marginTop:40,backgroundColor:'red'}} 
-                icon="send-check" 
-                mode="contained" 
-                //onPress={handleSubmit(onSubmit)}
-                >
-                  Ingresar
-              </Button>
-            
-            <Text style={{marginTop:25,color:"#b5b5b5"}}>Terminos y Condiciones</Text>
+      <Button
+        style={{ marginTop: 40, backgroundColor: 'red' }}
+        icon="login"
+        mode="contained"
+        onPress={() => {
+          if (email != '' && password != '') {
+            let findUser = users.find(usr => usr.useremail == email && usr.password == password);
+            if (findUser != undefined) {
+              seterrormess('')
+              const { name, email } = findUser
+              setEmail('');
+              setpassword('');
+              navigation.navigate('Contacts', { name: name, email: email })
+            }
+            else {
+              seterrormess('Correo y/o contraseña ICONRRECTO')
+            }
+          } else {
+            seterrormess('Todos los datos son obligatorios')
+          }
+        }}
+      >
+        Iniciar Sesion
+      </Button>
+      <Text style={{ color: 'green' }}>{vacio}</Text>
+      <Text style={{ color: 'red' }}>{errormess}</Text>
+
+      <Text style={{ marginTop: 25, color: "#b5b5b5" }}>Terminos y Condiciones</Text>
     </View>
   );
 }
-function ProductsScreen({navigation}){
-  let title="Este es el titulo"
+function ProductsScreen({ navigation }) {
+  let title = "Este es el titulo"
   let fullname = "Roxy la mejor de lo mejor"
-  return(
+  return (
     <View style={styles.container}>
-      <Text style={{marginBottom:20}}>Estamos en Productos</Text>
+      <Text style={{ marginBottom: 20 }}>Estamos en Productos</Text>
     </View>
   );
 }
 
-function HomeTabs(){
-  return(
+function HomeTabs() {
+  return (
     <Tab.Navigator
       screenOptions={{
-        headerShown:false, //lo que hace es no mostrar cada tittulo del componente....
-        tabBarActiveTintColor:'Blue',
-        tabBarInactiveTintColor:'white',
-        tabBarActiveBackgroundColor:'#b5b5b5',
-        tabBarInactiveBackgroundColor:'#8b0000'
+        headerShown: false, //lo que hace es no mostrar cada tittulo del componente....
+        tabBarActiveTintColor: 'Blue',
+        tabBarInactiveTintColor: 'white',
+        tabBarActiveBackgroundColor: '#b5b5b5',
+        tabBarInactiveBackgroundColor: '#8b0000'
       }}
     >
       <Tab.Screen name='Home' component={HomeScreen} options={{
-        // tabBarStyle:{display:'none'},
-        tabBarIcon:(tabInfo) => (<MaterialIcons name='home' size={22}/>)
-      }}/>
+        tabBarStyle: { display: 'none' },
+        tabBarIcon: (tabInfo) => (<MaterialIcons name='home' size={22} />)
+      }} />
       <Tab.Screen name='Products' component={ProductsScreen} options={{
-        tabBarIcon:(tabInfo) => (<MaterialIcons name='category' size={22}/>)
-      }}/>
+        tabBarIcon: (tabInfo) => (<MaterialIcons name='category' size={22} />)
+      }} />
       <Tab.Screen name='Contacts' component={Contacts} options={{
-        tabBarIcon:(tabInfo) => (<MaterialIcons name='call' size={22}/>)
-      }}/>
+        tabBarIcon: (tabInfo) => (<MaterialIcons name='call' size={22} />)
+      }} />
     </Tab.Navigator>
   );
 }
 
-      
+
 
 const styles = StyleSheet.create({
   container: {
@@ -125,9 +152,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  imgstyle:{
-    width:120,
-    height:100,
-    marginBottom:35
+  imgstyle: {
+    width: 120,
+    height: 100,
+    marginBottom: 35
   }
 });
